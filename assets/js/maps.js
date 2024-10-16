2d84defba5113cde24f84978fc50c8f8860f4289
Hedgemonkey <hedgemonkey@gmail.com>
2024-10-16 01:36:00 +0100

    Add google maps with markers

---
+++ assets/js/maps.js
@@ -0,0 +1,57 @@
+import { MarkerClusterer } from "https://cdn.skypack.dev/@googlemaps/markerclusterer@2.3.1";
+
+// Load the Google Maps JavaScript API.
+async function initMap() {
+    // Request needed libraries.
+    const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+    const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
+        "marker",
+    );
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        },
+        mapId: "MarkerCluster map"
+    });
+    const infoWindow = new google.maps.InfoWindow({
+        content: "",
+        disableAutoPan: true,
+    });
+    // Create an array of alphabetical characters used to label the markers.
+    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // Add some markers to the map.
+    const markers = locations.map((position, i) => {
+        const label = labels[i % labels.length];
+        const pinGlyph = new google.maps.marker.PinElement({
+            glyph: label,
+            glyphColor: "blue",
+        });
+        const marker = new google.maps.marker.AdvancedMarkerElement({
+            position,
+            content: pinGlyph.element,
+        });
+
+        // markers can only be keyboard focusable when they have click listeners
+        // open info window when marker is clicked
+        marker.addListener("click", () => {
+            infoWindow.setContent(position.lat + ", " + position.lng);
+            infoWindow.open(map, marker);
+        });
+        return marker;
+    });
+
+    // Add a marker clusterer to manage the markers.
+    new MarkerClusterer({ markers, map });
+}
+
+const locations = [
+    { lat: 40.785091, lng: -73.968285 },
+    { lat: 41.084045, lng: -73.874245 },
+    { lat: 40.754932, lng: -73.984016 }
+];
+
+initMap();
+
+//
